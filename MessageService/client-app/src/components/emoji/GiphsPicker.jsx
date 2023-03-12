ccea89ff4ac30b67fa6fc1196ff83438454ce48b
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2023-03-13 00:47:24 +0200

    npm packages fix

--- MessageService/client-app/src/components/emoji/GiphsPicker.jsx
+++ MessageService/client-app/src/components/emoji/GiphsPicker.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import ReactGiphySearchbox from 'react-giphy-searchbox'
 
 function GiphsPicker() {
-  return (
-    <ReactGiphySearchbox
-      apiKey='cwoInt0vXZ7t3VboAuk7Th1pYeciyjII'
-      onSelect={(item) => console.log(item)}
-      masonryConfig={[{ columns: 2, imageWidth: 175, gutter: 5 }]}
-      gifListHeight='350px'
-    />
-  )
+  return <div>Giphs</div>
 }
 
 export default GiphsPicker
