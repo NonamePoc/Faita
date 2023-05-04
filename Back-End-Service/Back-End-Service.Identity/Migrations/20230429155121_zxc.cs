f515b08f0d16b883f4b0c7bbb0d8f6b1c7f29936
NonamePoc <pavloshaban@gmail.com>
2023-05-04 19:19:00 +0300

    test

---
+++ Back-End-Service/Back-End-Service.Identity/Migrations/20230429155121_zxc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Back_End_Service.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class zxc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
