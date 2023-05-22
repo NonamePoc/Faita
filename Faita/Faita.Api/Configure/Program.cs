b4282f0519256923bbd3c9995801d4c9cc86b17e
NonamePoc <pavloshaban@gmail.com>
2023-05-22 15:10:46 +0300

    fix bug

--- Faita/Faita.Api/Configure/Program.cs
+++ Faita/Faita.Api/Configure/Program.cs
@@ -31,7 +31,33 @@ builder.Services.AddCors(options =>
             .AllowCredentials());
 });
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Faita.Api WEB API v1" }); });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+
+    // Другие настройки SwaggerGen
+});
+
 
 var app = builder.Build();
 
